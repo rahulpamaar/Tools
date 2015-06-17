ae42fa037815bb110a6cbb0bb805ca6ca9aff7b8
Chaitanya Sai Alaparthi <achaitanyasai@gmail.com>
2015-06-17 20:11:52 +0530

    permissions.sh added

---
+++ Permissions.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+
+# Change permissions of runner.sh and runner.rb
+
+sudo chmod 755 /var/lib/gems/1.9.1/gems/gettc-1.10/dist/template/bin/runner.sh
+sudo chmod 755 /var/lib/gems/1.9.1/gems/gettc-1.10/dist/template/bin/runner.rb
